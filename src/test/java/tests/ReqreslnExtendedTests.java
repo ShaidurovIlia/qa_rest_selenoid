5610a0f7e27a97e8c136c913fab7bfe385ed2e61
ShaidurovIlia <shaidurov.ilha@yandex.ru>
2023-08-10 17:06:04 +0200

    added allure

--- src/test/java/tests/ReqreslnExtendedTests.java
+++ src/test/java/tests/ReqreslnExtendedTests.java
@@ -1,14 +1,14 @@
 package tests;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import model.LoginBodyModel;
 import model.LoginResponseModel;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class ReqreslnExtendedTests {
@@ -53,5 +53,30 @@ public class ReqreslnExtendedTests {
 
         assertEquals("QpwL5tke4Pnpja7X4", loginResponse.getToken());
         //assertThat(loginResponse.getToken()).isEqualTo("QpwL5tke4Pnpja7X4");
+    }
+
+    @Test
+    void loginWithAllureTest() {
+
+        LoginBodyModel loginBody = new LoginBodyModel();
+        loginBody.setEmail("eve.holt@reqres.in");
+        loginBody.setPassword("cityslicka");
+
+        LoginResponseModel loginResponse =
+                given()
+                        .filter(new AllureRestAssured())
+                        .log().uri()
+                        .log().body()
+                        .contentType(JSON)
+                        .body(loginBody)
+                        .when()
+                        .post("https://reqres.in/api/login")
+                        .then()
+                        .log().status()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().as(LoginResponseModel.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", loginResponse.getToken());
     }
 }
